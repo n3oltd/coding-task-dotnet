da7d0b7bd28534634859bb3e35fe2dafec567494
Noor Drummond <noor.drummond@n3o.ltd>
2021-11-05 14:04:21 +0000

    Initial commit

---
+++ src/N3O.Challenge.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace N3O.Challenge.Web.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class UserController : Controller {
+        [HttpPost]
+        public IActionResult CreateUser() {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers() {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser() {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteUser() {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult PutUser() {
+            return Ok();
+        }
+    }
+}
